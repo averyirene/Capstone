e6814fd2171101a579108987b63f60f234ecbfce
Avery Stewart <averyirenes@gmail.com>
2024-08-24 16:42:10 +0100

    Update class rendering

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,11 +11,11 @@ const Header = ({ isLoggedIn }) => {
             <Link to="/"><img className="logo" src={logo} alt="Reassure logo" /></Link>
 
             <div className="links__container">
-                <NavLink to="/" className="nav__button" activeClassName="active">Home</NavLink>
-                <NavLink to="/breathe" className="nav__button" activeClassName="active">Breathe</NavLink>
-                <NavLink to="/about" className="nav__button--about" activeClassName="active">About</NavLink>
-                {!isLoggedIn && <NavLink to="/login" className="nav__button" activeClassName="active">Login</NavLink>}
-                {isLoggedIn && <NavLink to="/profile" className="nav__button"activeClassName="active">Profile</NavLink>}
+                <NavLink to="/" className={({ isActive }) => isActive ? "nav__button active" : "nav__button"}>Home</NavLink>
+                <NavLink to="/breathe" className={({ isActive }) => isActive ? "nav__button active" : "nav__button"}>Breathe</NavLink>
+                <NavLink to="/about" className={({ isActive }) => isActive ? "nav__button--about active" : "nav__button--about"}>About</NavLink>
+                {!isLoggedIn && (<NavLink to="/login" className={({ isActive }) => isActive ? "nav__button active" : "nav__button"}>Login</NavLink>)}
+                {isLoggedIn && (<NavLink to="/profile" className={({ isActive }) => isActive ? "nav__button active" : "nav__button"}>Profile</NavLink>)}
             </div>
             </div>
         </nav>
